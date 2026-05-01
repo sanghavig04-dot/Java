0f87e50530258391fdb858e44eaaca54153e9a74
sanghavig04-dot <sanghavig04@gmail.com>
2026-05-01 19:42:40 +0530

    Create AppTest.java

---
+++ AppTest.java
@@ -0,0 +1,9 @@
+public class AppTest {
+    public static void main(String[] args) {
+        if (App.add(2, 3) == 5) {
+            System.out.println("Test Passed ✅");
+        } else {
+            System.out.println("Test Failed ❌");
+        }
+    }
+}
